33fb1d6ca05048c19080ddc4358287be714eef3b
Luke Miller <Luke Miller>
2017-02-21 19:55:14 -0500

    HW done

---
+++ server/server.js
@@ -0,0 +1,36 @@
+const http = require('http');
+const fs = require('fs');
+const socketio = require('socket.io');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const index = fs.readFileSync(`${__dirname}/../client/index.html`);
+
+const onRequest = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(index);
+  response.end();
+};
+
+const app = http.createServer(onRequest).listen(port);
+
+console.log(`Listening on localhost:${port}`);
+
+const io = socketio(app);
+
+const draws = { };
+
+io.on('connection', (socket) => {
+  socket.join('room1');
+
+  socket.emit('join', { id: socket.id, draws });
+
+  socket.on('sendUpdate', (data) => {
+    socket.broadcast.to('room1').emit('receiveUpdate', data);
+    draws[data.drawTime] = data;
+  });
+
+  socket.on('disconnect', () => {
+    socket.leave('room1');
+  });
+});
